c7322297d50afd136c64d61be48f9457e9d9c7e7
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2024-12-14 21:25:09 -0600

    Api url replaced

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    SERVER: 'https://localhost:7183',
-    API: 'https://localhost:7183/api'
+    SERVER: 'https://messageappapi-os7e.onrender.com',
+    API: 'https://messageappapi-os7e.onrender.com/api'
 };
